41e7f90328cd5e4db6e3918cbcd2210fbd8bb9d2
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2025-02-27 00:24:14 +0900

    タイトルのコンポーネント作成

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Title from "../components/Title";
+
 export default function Page() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Title />
       <button className="btn">Default</button>
     </>
   );
---
+++ components/Title.tsx
@@ -0,0 +1,9 @@
+const Title = () => {
+  return (
+    <h1 className="text-3xl font-bold underline" data-testid="title">
+      Hello world!
+    </h1>
+  );
+};
+
+export default Title;
