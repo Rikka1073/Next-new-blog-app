e09255394b79f77e97f669a5e22588d4f8de6769
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2025-02-27 13:30:12 +0900

    記事一覧のレスポンシブ対応

--- app/article/page.tsx
+++ app/article/page.tsx
@@ -24,7 +24,7 @@ const page = async () => {
 
   return (
     <>
-      <div className="grid grid-cols-4 gap-4">
+      <div className="grid md:grid-cols-3 gap-4 sm:grid-cols-2 px-[10%] xl:px-0 lg:grid-cols-4">
         {article &&
           article.map((data: Article) => {
             return (
@@ -37,14 +37,16 @@ const page = async () => {
                     />
                   </figure>
                   <div className="card-body h-1/2">
-                    <h2 className="card-title text-md">
+                    <h2 className="card-title">
                       {data.title.length > 30 ? data.title.substring(0, 30) + "..." : data.title}
                     </h2>
                     <div className="flex gap-2">
                       {/* <p className="flex-grow-0">Likes {data.likes_count}</p> */}
-                      <div className="badge badge-sm border-black">Likes {data.likes_count}</div>
+                      <div className="badge badge-sm border-black text-[10px] lg:text-xs">
+                        Likes {data.likes_count}
+                      </div>
                       {/* <p className="flex-grow-0 ">Views {data.page_views_count}</p> */}
-                      <div className="badge badge-sm border-black">
+                      <div className="badge badge-sm border-black text-[10px] lg:text-xs">
                         Views {data.page_views_count}
                       </div>
                     </div>
