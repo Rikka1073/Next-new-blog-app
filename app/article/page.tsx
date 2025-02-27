f694c923768377b417f61c38fca1d00aef1572c2
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2025-02-27 10:25:44 +0900

    記事一覧のUI調整　コンテンツ幅の指定の追加

--- app/article/page.tsx
+++ app/article/page.tsx
@@ -18,22 +18,24 @@ const page = async () => {
 
   return (
     <>
-      {article &&
-        article.map((data: Article) => {
-          return (
-            <div key={data.id} className="card bg-base-100 w-96 shadow-xl">
-              <figure>
-                <img
-                  src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                  alt="Shoes"
-                />
-              </figure>
-              <div className="card-body">
-                <h2 className="card-title">{data.title}</h2>
+      <div className="grid grid-cols-4 gap-4">
+        {article &&
+          article.map((data: Article) => {
+            return (
+              <div key={data.id} className="card bg-base-100 shadow-xl">
+                <figure>
+                  <img
+                    src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                    alt="Shoes"
+                  />
+                </figure>
+                <div className="card-body">
+                  <h2 className="card-title">{data.title}</h2>
+                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+      </div>
     </>
   );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,9 @@ import "../styles/globals.css";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div className="max-w-6xl mx-auto">{children}</div>
+      </body>
     </html>
   );
 }
