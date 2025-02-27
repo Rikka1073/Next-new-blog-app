c486ea4d1c7791951d28bbc85f394466eeecd8d8
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2025-02-27 16:33:39 +0900

    テキストの色の変更

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,15 +3,24 @@ import Link from "next/link";
 const Header = () => {
   return (
     <header>
-      <div className="mb-20 border-b-2 border-[#75593D] p-6">
+      <div className="mb-20 border-b-2 border-[#75593D] p-6 text-[#75593D]">
         <nav className="flex justify-center gap-4">
-          <Link href="/" className="text-xl">
+          <Link
+            href="/"
+            className="text-xl hover:underline hover:underline-offset-2"
+          >
             Home
           </Link>
-          <Link href="/article" className="text-xl">
+          <Link
+            href="/article"
+            className="text-xl hover:underline hover:underline-offset-2"
+          >
             Article
           </Link>
-          <Link href="/blog" className="text-xl">
+          <Link
+            href="/blog"
+            className="text-xl hover:underline hover:underline-offset-2"
+          >
             Blog
           </Link>
         </nav>
--- components/Title.tsx
+++ components/Title.tsx
@@ -1,6 +1,9 @@
 const Title = ({ children }) => {
   return (
-    <h1 className="text-2xl font-bold text-center mb-10" data-testid="title">
+    <h1
+      className="mb-10 text-center text-2xl font-bold text-[#75593D]"
+      data-testid="title"
+    >
       {children}
     </h1>
   );
